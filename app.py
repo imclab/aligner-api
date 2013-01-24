ed5fc1e1a6261f9b20cddaae3012f23e152f38ce
Gavin Hackeling <gavinhackeling@gmail.com>
2013-01-23 21:37:46 -0500

    added Tornado

---
+++ app.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+ShallowQA v0.5 Alder
+
+Modified on Mon Sep 24 19:23:50 2012
+
+@author: gavin hackeling
+
+"""
+import os
+import tornado.web
+import tornado.ioloop
+from tornado.options import define, options
+import aligner
+import json
+
+
+class CheckHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write('service online')
+
+
+class AlignHandler(tornado.web.RequestHandler):
+    def post(self):
+        #data = self.get_argument('p_token', 'No data received')
+        data_json = tornado.escape.json_decode(self.request.body)
+        p_tokens = data_json['p_tokens']
+        h_tokens = data_json['h_tokens']
+        weights = data_json['weights']
+        alignments, score = aligner.align(p_tokens, h_tokens, weights)
+        d = json.dumps([vars(a) for a in alignments], sort_keys=True, indent=4)
+        self.write(d)
+
+
+handlers = [
+            (r"/check", CheckHandler),
+            (r"/align", AlignHandler),
+            ]
+
+
+settings = dict(template_path=os.path.join(
+    os.path.dirname(__file__), "templates"))
+application = tornado.web.Application(handlers, **settings)
+define("port", default=8000, help="run on the given port", type=int)
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    application.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
